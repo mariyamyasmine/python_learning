98701b33226307af1f600a283c823847d5057688
mariyamyasmine <mariyamyasminesm@gmail.com>
2024-08-16 17:55:23 +0530

    Create game.py
    created a game for rock ,paper,scissor

---
+++ game.py
@@ -0,0 +1,25 @@
+option = ["rock", "paper", "scissor"]
+user1 = input("Enter your move (rock, paper, scissor): ") 
+user2 = input("Enter your move (rock, paper, scissor): ") 
+if user1 not in option:
+    print("Invalid input! Please choose rock, paper, or scissor.")
+    if user2 not in option:
+        print("Invalid input! Please choose rock, paper, or scissor")
+elif user1 == user2:
+    print("It's a tie!")
+elif(user1 == "rock" and user2 == "scissor"):
+    print("user1 wins!")
+elif(user1 == "paper" and user2 == "rock"): 
+    print("user1 wins!")
+elif(user1 == "scissor" and user2 == "paper"):
+    print("user1 wins!")
+elif(user1 == "rock" and user2 == "paper"):
+    print("user2 wins!")
+elif(user1 == "scissor" and user2 == "rock"):
+    print("user2 wins!")
+elif(user1 == "paper" and user2 == "scissor"):
+    print("user2 wins!")
+elif(user1 == "rock" and user2 == "paper"):
+    print("user2 wins!")
+else:
+        print("invalid input")
